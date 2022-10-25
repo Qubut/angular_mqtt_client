c25922d7b39f77eebd33badfb0f8a8143d626bf1
Qubut <s-aahmed@haw-landshut.de>
2022-10-25 05:21:39 +0200

    created Dockerfile and used yarn instead of npm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+	hmr: false,
+	http: {
+    apiUrl:'localhost:1883',
+    
+	},
+	mqtt: {
+		server: 'localhost',
+		protocol: "ws",
+		port: 8081
+	}
 };
